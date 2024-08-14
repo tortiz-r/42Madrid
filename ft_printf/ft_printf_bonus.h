e2bb74814c4e83b10d6b5619dfc5c54cfbeb791a
Tristan Ortiz Roset <tortiz-r@c1r10s1.42madrid.com>
2024-08-14 14:04:43 +0200

    added ft_printf/ and Makefile in info/ | ft_printf not functional nor finished

---
+++ ft_printf/ft_printf_bonus.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_bonus.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tortiz-r <tortiz-r@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/14 12:24:46 by tortiz-r          #+#    #+#             */
+/*   Updated: 2024/08/14 12:58:44 by tortiz-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_BONUS_H
+# define FT_PRINTF_BONUS_H
+
+# include "libft/libft.h"
+# include "ft_printf.h"
+
+#endif
