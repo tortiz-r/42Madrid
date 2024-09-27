302ba928c1a288e2707db424c9f979b70eec724b
Tristan Ortiz Roset <tortiz-r@c1r6s5.42madrid.com>
2024-09-27 21:18:30 +0200

    mod ft_printf.c | not functional

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,16 +6,16 @@
 /*   By: tortiz-r <tortiz-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 10:11:47 by tortiz-r          #+#    #+#             */
-/*   Updated: 2024/08/14 12:57:11 by tortiz-r         ###   ########.fr       */
+/*   Updated: 2024/09/27 17:42:06 by tortiz-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_printf(char const *str, ...)
 {
-	
-	va_start (args, num_args);
+	va_list	args;
+	va_start (args, str);
 	va_arg(args, int);
-	va_copy();
+	
 	va_end(args);
 
 	return (0);
