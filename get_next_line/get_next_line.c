397de6121ac9e800db0dbb7005c3e03a0dfddf2c
Tristan Ortiz Roset <tortiz-r@c1r12s1.42madrid.com>
2024-10-24 20:35:21 +0200

    mod gnl.c gnl.h, added leer.txt | lists not incorporated | not funcional

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,8 +6,58 @@
 /*   By: tortiz-r <tortiz-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 12:27:07 by tortiz-r          #+#    #+#             */
-/*   Updated: 2024/09/29 12:39:09 by tortiz-r         ###   ########.fr       */
+/*   Updated: 2024/10/24 20:29:59 by tortiz-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 50
+#endif
+
+char	*get_next_line(int fd)
+{
+	static int	read_pos;
+
+	read_pos = 0;
+	printf("\n--->get_next_line<---\n");
+	printf("La variable estática read_pos es: %i\n", read_pos);
+	read_line(fd, read_pos);
+	if (read_pos != -1)
+		return (NULL);
+	return (NULL);
+}
+
+int	read_line(int fd, int read_pos)
+{
+	char	*buffer;
+
+	buffer = malloc((size_t)BUFFER_SIZE);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (-read_pos -1);
+	}
+	printf("\n--->read_line<---\n");
+	printf("Buffer_size es: %i\n", BUFFER_SIZE);
+	read_pos += read(fd, buffer, BUFFER_SIZE);
+	printf("La string leída es: %s\n", buffer);
+	return (read_pos);
+}
+
+//ssize_t read(int fildes, void *buf, size_t nbyte);
+// para compilar:
+//cc -Wall -Werror -Wextra -D BUFFER_SIZE=42 *.c *.h
+
+int	main(void)
+{
+	int		fd;
+	char	*random;
+
+	fd = open("leer.txt", O_RDONLY);
+	random = get_next_line(fd);
+	random = get_next_line(fd);
+	printf("%i\n", *random);
+	return (0);
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,15 +6,21 @@
 /*   By: tortiz-r <tortiz-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 12:28:35 by tortiz-r          #+#    #+#             */
-/*   Updated: 2024/09/29 12:40:27 by tortiz-r         ###   ########.fr       */
+/*   Updated: 2024/10/24 20:05:24 by tortiz-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-char	*get_next_line(int fd)
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
+# include <sys/stat.h>
+# include <fcntl.h>
 
+char	*get_next_line(int fd);
+int		read_line(int fd, int read_pos);
 
 #endif
