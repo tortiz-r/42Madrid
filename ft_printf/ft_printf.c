c3d954803c327691a9c95eced0f5a4c56657899e
Tristan_PC_win <tortiz.roset@gmail.com>
2024-12-21 13:05:46 +0100

    ft_printf: mod printf function, works ph char, string | incomplete

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -160,7 +160,7 @@ void	print_placeholder(va_list args, char const *str, int pos, int ph_code)
 int main(void)
 {
 	char *str = "hola que ta%%l j\n";
-	printf("num_args es: %i", 97);
+	printf("num_args es: %i\n", 97);
 	ft_printf("num_args es: %s", "holi\n");
 	// printf("num_args es: %i", 5);
 	return (0);
