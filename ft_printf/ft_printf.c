f6ea5d972f132d77d66a0a607b48bd0dac736825
Tristan_PC_win <tortiz.roset@gmail.com>
2024-12-21 12:50:11 +0100

    ft_printf: mod printf function, trying to add ph string | incomplete

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -12,48 +12,48 @@
 
 # include "ft_printf.h"
 
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
-size_t	ft_strlen(const char *s)
-{
-	unsigned long	i;
+// void	ft_putchar_fd(char c, int fd)
+// {
+// 	write(fd, &c, 1);
+// }
+// size_t	ft_strlen(const char *s)
+// {
+// 	unsigned long	i;
 
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	unsigned long	i;
-	char			*src_cpy;
+// 	i = 0;
+// 	while (s[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
+// size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+// {
+// 	unsigned long	i;
+// 	char			*src_cpy;
 
-	i = 0;
-	src_cpy = (char *) src;
-	if (size == 0)
-		return ((unsigned long) ft_strlen(src_cpy));
-	while (i < (size - 1) && src_cpy[i] != '\0')
-	{
-		dst[i] = src_cpy[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return ((unsigned long) ft_strlen(src_cpy));
-}
-char	*ft_strdup(const char *s)
-{
-	char	*result;
-	size_t	s_len;
+// 	i = 0;
+// 	src_cpy = (char *) src;
+// 	if (size == 0)
+// 		return ((unsigned long) ft_strlen(src_cpy));
+// 	while (i < (size - 1) && src_cpy[i] != '\0')
+// 	{
+// 		dst[i] = src_cpy[i];
+// 		i++;
+// 	}
+// 	dst[i] = '\0';
+// 	return ((unsigned long) ft_strlen(src_cpy));
+// }
+// char	*ft_strdup(const char *s)
+// {
+// 	char	*result;
+// 	size_t	s_len;
 
-	s_len = ft_strlen(s);
-	result = (char *) malloc(s_len + 1);
-	if (result == NULL)
-		return (NULL);
-	ft_strlcpy(result, s, s_len + 1);
-	return (result);
-}
+// 	s_len = ft_strlen(s);
+// 	result = (char *) malloc(s_len + 1);
+// 	if (result == NULL)
+// 		return (NULL);
+// 	ft_strlcpy(result, s, s_len + 1);
+// 	return (result);
+// }
 
 int	ft_printf(char const *str, ...)
 {
