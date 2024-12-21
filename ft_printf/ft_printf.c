25cc8756f6db445ea4096058d4aee7f6b475d7ca
Tristan_PC_win <tortiz.roset@gmail.com>
2024-12-21 14:01:15 +0100

    ft_printf: mod printf function, trying to add ph u | incomplete

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_printf/ft_printf.h"
+# include "ft_printf.h"
 # include <limits.h>
 
 int	ft_printf(char const *str, ...)
