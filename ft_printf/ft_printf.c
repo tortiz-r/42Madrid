6b4751d58ea0d84e2eb39a0aca790c2ff3e23e54
Tristan_PC_win <tortiz.roset@gmail.com>
2024-12-21 13:14:24 +0100

    ft_printf: mod printf function, trying to add ph d or i | incomplete

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -120,7 +120,7 @@ int main(void)
 {
 	char *str = "hola que ta%%l j\n";
 	printf("num_args es: %i\n", 97);
-	ft_printf("num_args es: %s", "holi\n");
+	ft_printf("num_args es: %i", 97);
 	// printf("num_args es: %i", 5);
 	return (0);
 }
